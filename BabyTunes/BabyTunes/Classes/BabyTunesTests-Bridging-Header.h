c507c23c426557616985f5d9e667afc553c87a58
Alan Aherne <Alan.Aherne@gmail.com>
2015-02-22 16:02:43 +0100

    Added pods FBSnatshot, SwiftyJSON and Alamofire

---
+++ BabyTunes/BabyTunes/Classes/BabyTunesTests-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BabyTunes-Bridging-Header.h
+//  BabyTunes
+//
+//  Created by Alan Aherne Restore on 22.02.15.
+//  Copyright (c) 2015 CCDimensions. All rights reserved.
+//
+
+#ifndef BabyTunes_BabyTunes_Bridging_Header_h
+#define BabyTunes_BabyTunes_Bridging_Header_h
+
+#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
+
+#endif
---
+++ BabyTunes/Pods/Target Support Files/Pods-BabyTunes-Alamofire/Pods-BabyTunes-Alamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BabyTunes_Alamofire : NSObject
+@end
+@implementation PodsDummy_Pods_BabyTunes_Alamofire
+@end
