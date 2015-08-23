021c36379e34440879ee482f10a67a4256d6ed99
Alan Aherne <Alan.Aherne@gmail.com>
2015-08-23 20:02:43 +0200

    - removed obsolete images and cleaned up the BTViewController

--- BabyTunes/BabyTunes/Classes/BTViewController.swift
+++ BabyTunes/BabyTunes/Classes/BTViewController.swift
@@ -13,7 +13,7 @@ import Parse
 enum Language : Int {
     case ENGLAND, GERMANY, FRANCE, SPAIN
 
-    static let languageNames = [ENGLAND : "England", GERMANY : "Germany", FRANCE : "France", SPAIN : "Spain"]
+    static let languageNames = [ENGLAND : "english", GERMANY : "german", FRANCE : "french", SPAIN : "spanish"]
     static let languageMouseNames = [ENGLAND : "MouseDE", GERMANY : "MouseEN", FRANCE : "MouseFR", SPAIN : "MouseSP"]
     
     func languageName() -> String {
@@ -41,10 +41,10 @@ enum Language : Int {
     }
 }
 
-let songsDict = ["England": ["Baa Baa Black Sheep", "Hush Little Baby", "Mary Had A Little Lamb", "The Animals Went In Two By Two", "Im A Little Teapot", "Old Mac Donald Had A Farm", "The Grand Old Duke Of York", "Hickory Dickory Dock", "Incy Wincy Spider", "Oranges And Lemons", "Three Blind Mice", "Humpty Dumpty", "London Bridge Is Falling Down", "Sing A Song Of Sixpence"],
-    "Germany": ["Alle Voegel sind schon da", "Ein Vogel wollte Hochzeit machen", "Kommt ein Vogel geflogen", "Auf einem Baum ein Kuckuck", "Der Mond ist aufgegangen", "Es klappert die Muehle", "Oh du lieber Augustin", "Backe backe Kuchen", "Die Gedanken sind frei", "Es tanzt ein Bi-Ba-Butzemann", "Spannenlanger Hansel", "Bruederchen komm tanz mit mir", "Ein Maennlein steht im Walde", "Haensel und Gretel", "Weisst du wieviel Sternlein stehen"],
-    "Spain": ["A mi burro", "El cocherito,lere", "La Tarara", "Anton Pirulero", "El patio de mi casa", "La reina Berenguela", "Tanto vestido blanco", "Cu-Cu, cantaba la rana", "Estando el senor don gato", "Los pollitos", "Tengo una muneca", "Debajo de un boton", "Jugando al escondite", "Quisiera ser tan alta",	"Tres hojitas, Madre"],
-    "France" : ["A la claire fontaine", "Cadet Rouselle", "Gentil coqulicot", "Le grand cerf", "Alouette, gentille alouette", "Cest Gugusse", "Il etait un petit navire", "Lempreur et le petit prince", "Arlequin danse sa boutique", "Compere Guilleri", "Jean de la Lune", "Maman, les ptits bateau", "Au claire de la lune", "Jean petit qui danse", "Savez - vous planter les choux"]]
+let songsDict = ["english": ["Baa Baa Black Sheep", "Hush Little Baby", "Mary Had A Little Lamb", "The Animals Went In Two By Two", "Im A Little Teapot", "Old Mac Donald Had A Farm", "The Grand Old Duke Of York", "Hickory Dickory Dock", "Incy Wincy Spider", "Oranges And Lemons", "Three Blind Mice", "Humpty Dumpty", "London Bridge Is Falling Down", "Sing A Song Of Sixpence"],
+    "german": ["Alle Voegel sind schon da", "Ein Vogel wollte Hochzeit machen", "Kommt ein Vogel geflogen", "Auf einem Baum ein Kuckuck", "Der Mond ist aufgegangen", "Es klappert die Muehle", "Oh du lieber Augustin", "Backe backe Kuchen", "Die Gedanken sind frei", "Es tanzt ein Bi-Ba-Butzemann", "Spannenlanger Hansel", "Bruederchen komm tanz mit mir", "Ein Maennlein steht im Walde", "Haensel und Gretel", "Weisst du wieviel Sternlein stehen"],
+    "spanish": ["A mi burro", "El cocherito,lere", "La Tarara", "Anton Pirulero", "El patio de mi casa", "La reina Berenguela", "Tanto vestido blanco", "Cu-Cu, cantaba la rana", "Estando el senor don gato", "Los pollitos", "Tengo una muneca", "Debajo de un boton", "Jugando al escondite", "Quisiera ser tan alta",	"Tres hojitas, Madre"],
+    "french" : ["A la claire fontaine", "Cadet Rouselle", "Gentil coqulicot", "Le grand cerf", "Alouette, gentille alouette", "Cest Gugusse", "Il etait un petit navire", "Lempreur et le petit prince", "Arlequin danse sa boutique", "Compere Guilleri", "Jean de la Lune", "Maman, les ptits bateau", "Au claire de la lune", "Jean petit qui danse", "Savez - vous planter les choux"]]
 
 
 
@@ -54,7 +54,7 @@ class BTViewController: UIViewController, SphereMenuDelegate, UITableViewDelegat
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var languageMouseImageView: UIImageView!
     
-    var tableCellsArray = songsDict[Language.GERMANY.languageName()]
+    var tableCellsArray: [AnyObject] = []
     var menu : SphereMenu!
     
     override func viewDidLoad()
@@ -67,30 +67,19 @@ class BTViewController: UIViewController, SphereMenuDelegate, UITableViewDelegat
         }
         
         var query = PFQuery(className: "Song")
-        query.whereKey("language", equalTo: "english")
+        query.whereKey("language", equalTo: Language.GERMANY.languageName())
         
-        var numObjects : Int = 0 // the num return objects from query
-        
+        var numOfQueryObjects : Int = 0
 
         query.findObjectsInBackgroundWithBlock{
             (objects: [AnyObject]?, error: NSError?) -> Void in
 
             if error == nil {
-                println(objects)
-                //println(objects.count)
                 
-                //var temp: NSArray = objects as! NSArray
-                println(numObjects) // at this point the value = 1
-                
-                //                if let songs = results as? [Song] {
-                //    for song in songs {
-                //        println(song.objectId)
-                        
-                        //let lastSuccessfulLevel = object["lastSuccessfulLevel"]
-                        //let score = object["score"]
-                        
-                        //println("lastSuccessfulLevel = " + (lastSuccessfulLevel as String))// + ", Score = " + score)
-                //    }
+                if let myObjects = objects {
+                    self.tableCellsArray = myObjects
+                    self.tableView.reloadData()
+                }
             }
             else {
                 println("%@", error)
@@ -129,7 +118,7 @@ class BTViewController: UIViewController, SphereMenuDelegate, UITableViewDelegat
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableCellsArray!.count;
+        return tableCellsArray.count;
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -144,7 +133,9 @@ class BTViewController: UIViewController, SphereMenuDelegate, UITableViewDelegat
         
         let cell = tableView.dequeueReusableCellWithIdentifier("BTCell", forIndexPath: indexPath) as! BTCell
         
-        cell.titleLabel.text = tableCellsArray![indexPath.row]
+        let song = tableCellsArray[indexPath.row]
+        
+        cell.titleLabel.text = "XX" //song["title]
         cell.titleLabel.textColor = UIColor.whiteColor();
         cell.titleLabel.font = UIFont(name: "AmericanTypewriter-Bold", size: 18)
         
@@ -157,7 +148,7 @@ class BTViewController: UIViewController, SphereMenuDelegate, UITableViewDelegat
         
         cell.cellImage!.clipsToBounds = true
         
-        cell.cellImage!.image = UIImage(named: "\(tableCellsArray![indexPath.row])_THM.png")
+        cell.cellImage!.image = UIImage(named: "\(tableCellsArray[indexPath.row])_THM.png")
 
         return cell
     }
@@ -181,9 +172,8 @@ class BTViewController: UIViewController, SphereMenuDelegate, UITableViewDelegat
         if let languageIndex = Language(rawValue : index)
         {
             //AA    self.languageMouseImageView.image = languageIndex.languageMouseCharacterImage()
-            tableCellsArray = songsDict[languageIndex.languageName()]
+            //AA    tableCellsArray = songsDict[languageIndex.languageName()]
             self.tableView.reloadData()
-            
             
             let opts : UIViewAnimationOptions = .TransitionFlipFromLeft
             UIView.transitionWithView(self.languageMouseImageView, duration: 0.8, options: opts,
